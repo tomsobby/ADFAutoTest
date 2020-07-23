b6c6cbfb98b284e166d45a82fb40da8469be5196
tomsobby <44785922+tomsobby@users.noreply.github.com>
2020-07-23 14:20:03 -0400

    Add files via upload

--- ADFTest.ps1
+++ ADFTest.ps1
@@ -1,3 +1,15 @@
+class CustomPipelineRunResult 
+{
+    [String] $DataFactoryName
+    [String] $ResourceGroupName
+    [String] $PipelineName
+    [DateTime] $ExecutionStart
+    [int] $DurationinMs
+    [string] $Message
+    [string] $Status
+}
+
+
 function Get-Report {
     # This function should beinvoked after the Test-DataFactory function is completed. This is a dumb function that just traverses through the results
     # and spits out a string. To capture the result into a file, the output of this function can be redirected to a file stream
@@ -41,7 +53,24 @@ function Get-Report {
     if ($failedPipelines.Count -gt 0 ) { Write-Host "There were Failures - review the log file" } 
 }
 
+function MapToCustomResult()
+{
+    param (
+            [object] $adfResult
+          )
 
+    $result = New-Object CustomPipelineRunResult
+
+    $result.DataFactoryName = $adfResult.DatafactoryName
+    $result.ResourceGroupName = $adfResult.ResourceGroupName
+    $result.PipelineName = $adfResult.Name
+    #$result.ExecutionStart = $adfResult.ExecutionStart
+    $result.DurationinMs = $adfResult.DurationInMs
+    $result.Message = $adfResult.Message
+    $result.Status = $adfResult.Status
+
+    return $result
+}
 
 function Test-DataFactory
 {
@@ -52,7 +81,9 @@ function Test-DataFactory
           )
 
     process {
-    
+        
+        [Object] $resultToAdd
+
         # for production use uncomment the below and replace with Managed Service Identity call 
        # $credential = Get-Credential
         # Connect-AzAccount -Credential $credential
@@ -66,25 +97,54 @@ function Test-DataFactory
             $datafactoryName = $dataFactory.DAName
             $resourceGroupName = $datafactory.ResourceGroup
 
-	        $pipelines = Get-AzDataFactoryV2Pipeline -ResourceGroupName $resourceGroupName -DataFactoryName $datafactoryName
+	        #$pipelines = Get-AzDataFactoryV2Pipeline -ResourceGroupName $resourceGroupName -DataFactoryName $datafactoryName
+            $pipelines = $datafactory.pipelines
 
             foreach ($pipeline in $pipelines) {
                 Write-Host "Testing DataFactory $($datafactoryName) - Invoking Pipeline : $($pipeline.Name) "
+
         
-                $pipelineRunID = Invoke-AzDataFactoryV2Pipeline -ResourceGroupName $resourceGroupName -DataFactoryName $datafactoryName -PipelineName $pipeline.Name
+                if($pipelines.parameters.Count -ne 0) {
+                    #Convert Array to Hashtable as this is require to be passed to the InvokePipeline function
+                    $json = $pipelines.parameters | ConvertTo-Json
+                    $hashparam = @{}
+                    (ConvertFrom-Json $json).psobject.properties | Foreach { $hashparam[$_.Name] = $_.Value }
 
-                # loop through until the result is not In Progress. This may result in the script taking too long to execute. Alternatively, we can 
-                # terminate the loop after certain time threshold has been reached
-                do {
-                    Start-Sleep -s 5  # delay for a few seconds before checking again
-                    $result = Get-AzDataFactoryV2PipelineRun -ResourceGroupName $resourceGroupName -DataFactoryName $datafactoryName -PipelineRunId $pipelineRunID 
-		        } until ($result.Status -ne "InProgress")
+                    $pipelineRunID = Invoke-AzDataFactoryV2Pipeline -ResourceGroupName $resourceGroupName -DataFactoryName $datafactoryName -PipelineName $pipeline.Name -Parameter $hashparam
+                }
+                else {
+                    $pipelineRunID = Invoke-AzDataFactoryV2Pipeline -ResourceGroupName $resourceGroupName -DataFactoryName $datafactoryName -PipelineName $pipeline.Name
+                }
 
-                $total = $pipelineTestResults.Add($result)       
-            }
+                if ($pipelineRunID -ne $null) {          
+                    # loop through until the result is not In Progress. This may result in the script taking too long to execute. Alternatively, we can 
+                    # terminate the loop after certain time threshold has been reached
+                    do {
+                        Start-Sleep -s 5  # delay for a few seconds before checking again
+                        $result = Get-AzDataFactoryV2PipelineRun -ResourceGroupName $resourceGroupName -DataFactoryName $datafactoryName -PipelineRunId $pipelineRunID 
+		            } until ($result.Status -ne "InProgress")
+                    
+                    $resultToAdd = MapToCustomResult($result)                      
+                }     
+                else { # pipeline does not exists
+                    $resultToAdd = New-Object CustomPipelineRunResult
+                    $resultToAdd.DataFactoryName = $datafactoryName
+                    $resultToAdd.ResourceGroupName = $resourceGroupName
+                    $resultToAdd.PipelineName = $pipeline.Name
+                    $resultToAdd.ExecutionStart = Get-Date
+                    $resultToAdd.DurationinMs = 0
+                    $resultToAdd.Status = "Failed"
+                    $resultToAdd.Message = "Pipeline not found. 1) Check name in config and, 2) If the pipeline has been deployed"
+
+                } #end of if (pipelineRunID -ne $null)
+
+                $total = $pipelineTestResults.Add($resultToAdd)  
+
+            } #end of for each
         }   
     }
 }
+
 
 
 # This is where it all begins :-) !!
